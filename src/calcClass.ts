1b223fb3e2a4fdc7b4e34c46d14b2c6710d0a7b1
Athina <athina.kantis@gmail.com>
2025-02-06 20:34:47 +0200

    add Calculator class

--- src/calcClass.ts
+++ src/calcClass.ts
@@ -1,6 +1,75 @@
-'use strict';
+import { Calc } from './types/types';
 
-export const add = (a: number, b: number): number => a + b;
-export const subtract = (a: number, b: number): number => a - b;
-export const divide = (a: number, b: number): number => a / b;
-export const multiply = (a: number, b: number): number => a * b;
+/*
+A simple calculator with some error handling
+
+*/
+export class Calculator {
+  #a;
+  #b;
+  #operation;
+
+  constructor(
+    a: number | null = null,
+    b: number | null = null,
+    operation: Calc | null = null
+  ) {
+    this.#a = a;
+    this.#b = b;
+    this.#operation = operation;
+  }
+
+  get a() {
+    if (this.#a === null) {
+      throw new Error('Missing input');
+    }
+    return this.#a;
+  }
+
+  get b() {
+    if (this.#b === null) {
+      throw new Error('Missing input');
+    }
+    return this.#b;
+  }
+
+  get operation() {
+    return this.#operation as Calc;
+  }
+
+  set a(value: number | null) {
+    this.#a = value;
+  }
+
+  set b(value: number | null) {
+    this.#b = value;
+  }
+
+  set operation(s: Calc | null) {
+    this.#operation = s;
+  }
+
+  calculate() {
+    if (this.#a === null || this.#b === null) return;
+
+    switch (this.#operation) {
+      case '+':
+        return this.#a + this.#b;
+      case '-':
+        return this.#a - this.#b;
+      case '/':
+        if (this.#b === 0) {
+          throw new Error('Cannot divide by zero')
+        }
+        return this.#a / this.#b;
+      case '*':
+        return this.#a * this.#b;
+    }
+  }
+
+  reset() {
+    this.#a = null
+    this.#b = null
+    this.#operation = null
+  }
+}
