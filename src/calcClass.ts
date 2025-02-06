9f1e17e47fb3b8c9d6eb43fc447bd08d95c563fa
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-02-06 16:15:21 +0200

    update calc

--- src/calcClass.ts
+++ src/calcClass.ts
@@ -1,58 +1,6 @@
 'use strict';
 
-export class Calculator {
-  #valueA: number | undefined;
-  #valueB: number | undefined;
-  #operation: string | null;
-
-  constructor(operation = null, valueA = undefined, valueB = undefined) {
-    this.#valueA = valueA;
-    this.#valueB = valueB;
-    this.#operation = operation;
-  }
-
-  get valueA() {
-    return this.#valueA;
-  }
-
-  get valueB() {
-    return this.#valueB;
-  }
-
-  get operation() {
-    return this.#operation;
-  }
-
-  updateValueA(newValue: number) {
-    this.#valueA = newValue;
-  }
-
-  updateValueB(newValue: number) {
-    this.#valueB = newValue;
-  }
-
-  updateOperation(newOp: string) {
-    this.#operation = newOp;
-  }
-
-  calculate() {
-    if (!this.#valueA || !this.#valueB) return;
-
-    switch (this.#operation) {
-      case '+':
-        return this.#valueA + this.#valueB;
-      case '-':
-        return this.#valueA - this.#valueB;
-      case '/':
-        return this.#valueA / this.#valueB;
-      case '*':
-        return this.#valueA * this.#valueB;
-    }
-  }
-
-  resetCalculator() {
-    this.#valueA = undefined;
-    this.#valueB = undefined;
-    this.#operation = null;
-  }
-}
+export const add = (a: number, b: number): number => a + b;
+export const subtract = (a: number, b: number): number => a - b;
+export const divide = (a: number, b: number): number => a / b;
+export const multiply = (a: number, b: number): number => a * b;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css';
-import { Calculator } from './calcClass';
+import { add, subtract, divide, multiply } from './calcClass';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <main>
@@ -41,103 +41,124 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </main>
 `;
 
+type Calc = '+' | '-' | '*' | '/';
+let a: number, b: number, operation: Calc;
+
 document.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('input');
-  const calculator = new Calculator();
-  const calcBtns = document.querySelectorAll('.btns button');
-  const calcP = document.querySelector('#calculation');
+    const input: HTMLInputElement = document.querySelector('input')!; // Exclamation mark at the end means 'This will definitely not be null'
+    const calcP: HTMLParagraphElement = document.querySelector('#calculation')!;
+    const calcBtns: NodeListOf<HTMLButtonElement> =
+        document.querySelectorAll('.btns button');
 
-  const handleMouseDown = (e) => {
-    animateDown(e);
-    const { numId } = e.target.dataset;
+    const handleMouseDown = (e) => {
+        animateDown(e);
+        const { numId } = e.target.dataset;
 
-    if (numId.match(/[*\-+/]/g)) {
-      if (input!.value.length < 1) return;
-      calculator.updateOperation(numId);
-      calculator.updateValueA(+input!.value.split(numId)[0]);
-      console.log(calculator.valueA);
+        if (numId.match(/[*\-+/]/g)) {
+            if (input!.value.length < 1) return;
+            operation = numId;
+            a = +input!.value.split(numId)[0];
 
-      if (input!.value.match(/[*\-+/]/g)) {
-        return (input!.value = input!.value.replace(/[*\-+/]/g, numId));
-      }
-      return (input!.value += numId);
-    }
+            if (input!.value.match(/[*\-+/]/g)) {
+                return (input!.value = input!.value.replace(/[*\-+/]/g, numId));
+            }
+            return (input!.value += numId);
+        }
 
-    numId === '=' ? handleCalculate() : (input!.value += numId);
-  };
+        numId === '=' ? handleCalculate() : (input!.value += numId);
+    };
 
-  const handleCalculate = () => {
-    const match = input!.value.match(/(?<=[+\-*/])[^+\-*/=]+(?==)/g);
-    if (!match) return;
+    const handleCalculate = () => {
+        console.log('calculating');
 
-    calculator.updateValueB(+match[0]);
-    console.log(calculator.valueA);
-    console.log(calculator.valueB);
+        b = +input!.value.split(operation)[1];
 
-    calcP!.textContent = `${calculator.valueA} ${calculator.operation} ${calculator.valueB} =`
-    input!.value = calculator.calculate()!.toString();
-  };
+        calcP!.textContent = `${a} ${operation} ${b} =`;
+        input!.value = calc(operation).toString();
+        console.log(calc(operation));
+    };
 
+    const calc = (operation: string): number => {
+        switch (operation) {
+            case '+':
+                return add(a, b);
+            case '-':
+                return subtract(a, b);
+            case '/':
+                return divide(a, b);
+            case '*':
+                return multiply(a, b);
+            default:
+                return 0;
+        }
+    };
 
-  // Attach event listeners to buttons
-  calcBtns.forEach((btn) => {
-    btn.addEventListener('mousedown', handleMouseDown);
-    btn.addEventListener('mouseup', animateUp);
-  });
+    // Attach event listeners to buttons
+    calcBtns.forEach((btn) => {
+        btn.addEventListener('mousedown', handleMouseDown);
+        btn.addEventListener('mouseup', animateUp);
+    });
 
-  // Add event listeners to keyboard events
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      input!.value = input!.value.substring(0, input!.value.length - 1);
-      new Audio(`/audio/click2.mp3`).play();
-    }
+    // Add event listeners to keyboard events
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            calcP!.textContent = '';
+            input!.value = input!.value.substring(0, input!.value.length - 1);
+            new Audio(`/audio/click2.mp3`).play();
+        }
 
-    let button = document.querySelector(`button[data-num-id='${e.key}']`);
+        let button = document.querySelector(`button[data-num-id='${e.key}']`);
 
-    if (e.key === 'Enter') {
-      button = document.querySelector(`button[data-num-id='=']`);
-    }
+        if (e.key === 'Enter') {
+            document
+                .querySelector(`button[data-num-id='=']`)
+                ?.dispatchEvent(new MouseEvent('mousedown'));
+        }
 
-    if (button) {
-      const mousedownEvent = new MouseEvent('mousedown', {
-        bubbles: true,
-        cancelable: true,
-        view: window,
-      });
-      button.dispatchEvent(mousedownEvent);
-    }
-  });
+        if (e.key === '.' || e.key === ',') {
+            if (input!.value.length - 1 === '.') return;
+        }
 
-  document.addEventListener('keyup', (e) => {
-    let button = document.querySelector(`button[data-num-id='${e.key}']`);
+        if (button) {
+            const mousedownEvent = new MouseEvent('mousedown', {
+                bubbles: true,
+                cancelable: true,
+                view: window,
+            });
+            button.dispatchEvent(mousedownEvent);
+        }
+    });
 
-    if (e.key === 'Enter') {
-      button = document.querySelector(`button[data-num-id='=']`);
-    }
+    document.addEventListener('keyup', (e) => {
+        let button = document.querySelector(`button[data-num-id='${e.key}']`);
 
-    if (button) {
-      const mouseupEvent = new MouseEvent('mouseup', {
-        bubbles: true,
-        cancelable: true,
-        view: window,
-      });
-      button.dispatchEvent(mouseupEvent);
-    }
-  });
+        if (e.key === 'Enter') {
+            button = document.querySelector(`button[data-num-id='=']`);
+        }
+
+        if (button) {
+            const mouseupEvent = new MouseEvent('mouseup', {
+                bubbles: true,
+                cancelable: true,
+                view: window,
+            });
+            button.dispatchEvent(mouseupEvent);
+        }
+    });
 });
 
-  // Animations + Audio cues
-  interface MouseEventWithTarget extends MouseEvent {
+// Animations + Audio cues
+interface MouseEventWithTarget extends MouseEvent {
     target: HTMLButtonElement;
-  }
+}
 
-  const animateDown = (e: MouseEventWithTarget): void => {
+const animateDown = (e: MouseEventWithTarget): void => {
     e.target.style.backgroundImage = 'url(/button_pressed.png)';
     e.target.style.transform = 'translate(0px, 7px)';
     new Audio(`/audio/click1.mp3`).play();
-  };
+};
 
-  const animateUp = (e: MouseEventWithTarget) => {
+const animateUp = (e: MouseEventWithTarget) => {
     e.target.style.backgroundImage = 'url(/button.png)';
     e.target.style.transform = 'translate(0px, 0px)';
-  };
+};
