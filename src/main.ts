e18c9e37d7cc5a8547fed0334de73e2584f1759f
[Athina Kantis] <[athina.kantis@gmail.com]>
2025-02-07 09:37:42 +0200

    clear calcP when altering the result value.

--- src/main.ts
+++ src/main.ts
@@ -53,109 +53,113 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const input = document.querySelector('input');
-  const numBtns = document.querySelectorAll(`button[data-num-type='num']`);
-  const calcBtns = document.querySelectorAll(`button[data-num-type='calc']`);
-  const calcP = document.querySelector('#calculation')!;
+    const input = document.querySelector('input');
+    const numBtns = document.querySelectorAll(`button[data-num-type='num']`);
+    const calcBtns = document.querySelectorAll(`button[data-num-type='calc']`);
+    const calcP = document.querySelector('#calculation')!;
 
-  const calculator = new Calculator();
+    const calculator = new Calculator();
 
-  // Add event listeners for all numerical buttons [1-9]
-  calcBtns.forEach((btn) => {
-    btn.addEventListener('mousedown', (e) => {
-      animateDown(e as MouseEventWithTarget);
-      if (input!.value.length < 1) return;
-      calculator.a = +input!.value.split(calculator.operation!)[0];
-      const operation = btn.getAttribute('data-num-id');
-      calculator.operation = operation as Calc;
+    // Add event listeners for all numerical buttons [1-9]
+    calcBtns.forEach((btn) => {
+        btn.addEventListener('mousedown', (e) => {
+            animateDown(e as MouseEventWithTarget);
+            if (input!.value.length < 1) return;
+            calculator.a = +input!.value.split(calculator.operation!)[0];
+            const operation = btn.getAttribute('data-num-id');
+            calculator.operation = operation as Calc;
 
-      const match = input!.value.match(/[*\-+/]/g);
+            const match = input!.value.match(/[*\-+/]/g);
 
-      match
-        ? (input!.value = input!.value.replace(
-            match as unknown as string,
-            operation as string
-          ))
-        : (input!.value += operation);
+            match
+                ? (input!.value = input!.value.replace(
+                      match as unknown as string,
+                      operation as string
+                  ))
+                : (input!.value += operation);
+        });
+        btn.addEventListener('mouseup', (e) =>
+            animateUp(e as MouseEventWithTarget)
+        );
     });
-    btn.addEventListener('mouseup', (e) =>
-      animateUp(e as MouseEventWithTarget)
+
+    // Add event listeners for calculation
+    const calculateBtn = document.querySelector(
+        `button[data-num-type='calculate']`
+    )!;
+    calculateBtn.addEventListener('mousedown', async (e) => {
+        animateDown(e as MouseEventWithTarget);
+        const valueB = input!.value.split(calculator.operation!)[1];
+        if (!valueB) return;
+        calculator.b = +valueB;
+        const result = calculator.calculate();
+        calcP.textContent = `${calculator.a} ${calculator.operation} ${calculator.b} =`;
+        input!.value = result!.toString();
+        calculator.a = result!;
+        calculator.b = null;
+        calculator.operation = null;
+    });
+    calculateBtn.addEventListener('mouseup', (e) =>
+        animateUp(e as MouseEventWithTarget)
     );
-  });
 
-  // Add event listeners for calculation
-  const calculateBtn = document.querySelector(
-    `button[data-num-type='calculate']`
-  )!;
-  calculateBtn.addEventListener('mousedown', async (e) => {
-      animateDown(e as MouseEventWithTarget);
-      const valueB = input!.value.split(calculator.operation!)[1];
-      if (!valueB) return;
-      calculator.b = +valueB;
+    numBtns.forEach((btn) => {
+        btn.addEventListener('mousedown', (e) => {
+            animateDown(e as MouseEventWithTarget);
+        });
 
-      const result = calculator.calculate();
-      calcP.textContent = `${calculator.a} ${calculator.operation} ${calculator.b} =`;
-      input!.value = result!.toString();
-      calculator.a = result!;
-      calculator.b = null;
-      calculator.operation = null;
-  });
-  calculateBtn.addEventListener('mouseup', (e) =>
-    animateUp(e as MouseEventWithTarget)
-  );
-
-  numBtns.forEach((btn) => {
-    btn.addEventListener('mousedown', (e) => {
-      animateDown(e as MouseEventWithTarget);
+        btn.addEventListener('mouseup', (e) =>
+            animateUp(e as MouseEventWithTarget)
+        );
     });
 
-    btn.addEventListener('mouseup', (e) =>
-      animateUp(e as MouseEventWithTarget)
+    const comma = document.querySelector(`button[data-num-type='comma']`)!;
+    comma.addEventListener('mousedown', (e) => {
+        animateDown(e as MouseEventWithTarget);
+        if (input!.value.length < 1) return;
+        if (input!.value[input!.value.length - 1] === '.') return;
+        input!.value += '.';
+    });
+    comma.addEventListener('mouseup', (e) =>
+        animateUp(e as MouseEventWithTarget)
     );
-  });
 
-  const comma = document.querySelector(`button[data-num-type='comma']`)!;
-  comma.addEventListener('mousedown', (e) => {
-    animateDown(e as MouseEventWithTarget);
-    if (input!.value.length < 1) return;
-    if (input!.value[input!.value.length - 1] === '.') return;
-    input!.value += '.';
-  });
-  comma.addEventListener('mouseup', (e) =>
-    animateUp(e as MouseEventWithTarget)
-  );
-
-  numBtns.forEach((btn) => {
-    btn.addEventListener('mousedown', (e) => {
-      animateDown(e as MouseEventWithTarget);
-      input!.value += btn.getAttribute('data-num-id');
+    numBtns.forEach((btn) => {
+        btn.addEventListener('mousedown', (e) => {
+            animateDown(e as MouseEventWithTarget);
+            if (calcP.textContent!.length > 0) calcP.textContent = '';
+            input!.value += btn.getAttribute('data-num-id');
+        });
     });
-  });
 
-  document.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      input!.value.length < 2 ? reset() : input!.value = input!.value.substring(0, input!.value.length - 1)
-      new Audio(`/audio/click2.mp3`).play();
-    }
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            new Audio(`/audio/click2.mp3`).play();
+            if (input!.value.length < 2) {
+                reset();
+            }
+            input!.value = input!.value.substring(0, input!.value.length - 1);
+            calcP.textContent = '';
+        }
 
-    if (e.key === 'Enter') {
-      calculateBtn.dispatchEvent(new MouseEvent('mousedown'));
-    }
+        if (e.key === 'Enter') {
+            calculateBtn.dispatchEvent(new MouseEvent('mousedown'));
+        }
 
-    const button = document.querySelector(`button[data-num-id="${e.key}"]`);
-    button?.dispatchEvent(new MouseEvent('mousedown'));
-  });
+        const button = document.querySelector(`button[data-num-id="${e.key}"]`);
+        button?.dispatchEvent(new MouseEvent('mousedown'));
+    });
 
-  document.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter')
-      calculateBtn.dispatchEvent(new MouseEvent('mouseup'));
-    const button = document.querySelector(`button[data-num-id='${e.key}']`);
-    button?.dispatchEvent(new MouseEvent('mouseup'));
-  });
+    document.addEventListener('keyup', (e) => {
+        if (e.key === 'Enter')
+            calculateBtn.dispatchEvent(new MouseEvent('mouseup'));
+        const button = document.querySelector(`button[data-num-id='${e.key}']`);
+        button?.dispatchEvent(new MouseEvent('mouseup'));
+    });
 
-  const reset = () => {
-    calculator.reset();
-    input!.value = '';
-    calcP.textContent = '';
-  };
+    const reset = () => {
+        calculator.reset();
+        input!.value = '';
+        calcP.textContent = '';
+    };
 }
